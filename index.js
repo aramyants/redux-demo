bea356b33d320ee038456b417029364ff97aaf91
kamalyanaram <aramaramyants@gmail.com>
2025-07-16 18:44:19 +0400

    chore: Introduction to Redux and implementing base architecture pattern

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('Redux Demo - Index File');
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+
+function restockCake(quantity = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: quantity,
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+  anotherProperty: 'example',
+};
+
+// (previousState = {}, action) => newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - action.payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state', store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(3));
+
+unsubscribe(); // Unsubscribe from the store updates
+console.log('Unsubscribed from store updates');
