929f7973bed10f76cb1e66ba60c1cfa6b65efa0e
kamalyanaram <aramaramyants@gmail.com>
2025-07-17 11:38:16 +0400

    Seperate reducers stores and etc

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
 function orderCake() {
   return {
@@ -18,14 +22,36 @@ function restockCake(quantity = 1) {
   };
 }
 
-const initialState = {
+function orderIceCream() {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: 1,
+  };
+}
+
+function restockIceCream(quantity = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: quantity,
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+const initialCakeState = {
   numOfCakes: 10,
-  anotherProperty: 'example',
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
 };
 
 // (previousState = {}, action) => newState
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
@@ -37,22 +63,56 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes + action.payload,
       };
+
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+        return {
+          ...state,
+          numOfIceCreams: state.numOfIceCreams - 1,
+        };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+
+const store = createStore(rootReducer);
 console.log('Initial state', store.getState());
 
 const unsubscribe = store.subscribe(() => {
   console.log('Updated state', store.getState());
 });
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(3));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators({orderCake, restockCake, orderIceCream, restockIceCream}, store.dispatch);
+
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3)
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
 
 unsubscribe(); // Unsubscribe from the store updates
 console.log('Unsubscribed from store updates');
